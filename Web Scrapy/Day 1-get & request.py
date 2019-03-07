f6736eab535ed1972c6b5ee8ebefb613cf861b73
Flash Wang <phynew0111@gmail.com>
2019-03-07 23:30:39 +0800

    Update Day 1-get & request.py

--- Web Scrapy/Day 1-get & request.py
+++ Web Scrapy/Day 1-get & request.py
@@ -4,6 +4,17 @@ reponse = request.get(url)
 print(reponse)
 
 from urllib.request import urlopen
+import re
 
 html = urlopen("https://morvanzhou.github.io/static/scraping/basic-structure.html").read().decode('utf-8')
 print(html)
+
+res = re.findall(r"<title>(.+?)</title>", html)
+print("\nPage title is: ", res[0])
+
+res1 = re.findall(r"<p>(.*?)</p>", html, flags=re.DOTALL)
+print("\nPage paragraph is: ", res1[0])
+
+# Find all links.
+res2 = re.findall(r'href="(.*?)"', html)
+print("\nAll links: ", res2)
