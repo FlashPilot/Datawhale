d68edbdc2c3011d4fb7aa262ae47ad8d730adf81
Flash Wang <phynew0111@gmail.com>
2019-03-07 23:09:09 +0800

    Update Day 1-get & request.py

--- Web Scrapy/Day 1-get & request.py
+++ Web Scrapy/Day 1-get & request.py
@@ -2,3 +2,8 @@ import requests
 url = 'https://www.baidu.com'
 reponse = request.get(url)
 print(reponse)
+
+from urllib.request import urlopen
+
+html = urlopen("https://morvanzhou.github.io/static/scraping/basic-structure.html").read().decode('utf-8')
+print(html)
